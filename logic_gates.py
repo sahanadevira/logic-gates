464690ef594802bc76f81f87244d9734c8963a50
Sahana devi R A <sahanadevi2037@gmail.com>
2026-04-29 22:56:34 +0530

    Add files via upload

---
+++ logic_gates.py
@@ -0,0 +1,72 @@
+print("---Binary Inputs for logic gate verification---")
+
+def AND():
+    n = int(input("Enter number of test cases to check: "))
+    for i in range(n):
+        a = int(input("Enter input_1: "))
+        b = int(input("Enter input_2: "))
+        print(a&b)
+
+
+def OR():
+    n = int(input("Enter number of test cases to check: "))
+    for i in range(n):
+        a = int(input("Enter input_1: "))
+        b = int(input("Enter input_2: "))
+        print(a|b)
+
+def NOT():
+    n = int(input("Enter number of test cases to check: "))
+    for i in range(n):
+        a = int(input("Enter input_1: "))
+        print(1 - a)        
+
+def NAND():
+    n = int(input("Enter number of test cases to check: "))
+    for i in range(n):
+        a = int(input("Enter input_1: "))
+        b = int(input("Enter input_2: "))
+        print(1 - (a&b))
+        
+def NOR():
+    n = int(input("Enter number of test cases to check: "))
+    for i in range(n):
+        a = int(input("Enter input_1: "))
+        b = int(input("Enter input_2: "))
+        print (1 - (a|b))
+
+def XOR():
+    n = int(input("Enter number of test cases to check: "))
+    for i in range(n):
+        a = int(input("Enter input_1: "))
+        b = int(input("Enter input_2: "))
+        print (a^b)        
+
+        
+        
+
+while True:
+    print("---select gate type to check---")
+    print("1.AND gate\n2.OR gate\n3.NOT gate\n4.NAND gate\n5.NOR gate\n6.X-OR gate\n7.Exit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        AND()
+    elif choice == 2:
+        OR()
+    elif choice == 3:
+        NOT()
+    elif choice == 4:
+        NAND()
+    elif choice == 5:
+        NOR()
+    elif choice == 6:
+        XOR()
+    elif choice == 7:
+        print("Exiting...")
+        break
+    else:
+        print("invalid choice")
+        
+        
+    
